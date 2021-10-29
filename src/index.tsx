d169fc3e1521f1764003fbc2124299f056918b53
szHeron <heronbr7@gmail.com>
2021-10-29 11:12:35 -0300

    added warning for mobile users

--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { isBrowser } from 'react-device-detect';
 import './index.css';
 import App from './App';
 import AuthContextProvider from './context/AuthContext';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <AuthContextProvider>
-      <App />
-    </AuthContextProvider>
+    {isBrowser?
+      <AuthContextProvider>
+        <App/>
+      </AuthContextProvider>
+    :
+      <h1>For now the site is only available for desktops</h1>
+  }
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
