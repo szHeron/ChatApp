3c22288a1407953bb2bc8cb15aa9710fbdc55919
szHeron <heronbr7@gmail.com>
2021-11-03 13:16:25 -0300

    added back button

--- src/pages/ChangeProfile/index.tsx
+++ src/pages/ChangeProfile/index.tsx
@@ -63,7 +63,7 @@ export default function ChangeProfileInfos(){
         <Content>
             <Aside>
                 <img height="420" width="420" src="./FinishProfile.svg" alt="chat"/>
-                <BackButton>
+                <BackButton onClick={()=>history.push('/')}>
                     <svg width="24" height="24" fill="#fff" xmlns="http://www.w3.org/2000/svg" fill-rule="evenodd" clip-rule="evenodd"><path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z"/></svg>
                     Voltar
                 </BackButton>
